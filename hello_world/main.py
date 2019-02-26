352d2ca05f2c1d332716b197eb5ab36cc64d907e
Martin Ondejka <martin.ondejka@vacuumlabs.com>
2019-02-26 13:27:05 +0100

    Revert "add counter and structure to the application"
    This reverts commit 3335464faf92aba428e548b633a2d28c94c04cc6.

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,16 +1,11 @@
 from sanic import Sanic
 from sanic.response import json
-from sanic.response import text
-from sanic import Blueprint
-from counter import counter_bp
 
 app = Sanic()
-app.blueprint(counter_bp)
 
 @app.route("/")
 async def test(request):
-    return text("OK")
+    return json({"hello": "world"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
